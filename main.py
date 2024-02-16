ddb433a33bc79c24cba34e4a226f36430a70d4bf
Gabe-Matherley <157724600+Gabe-Matherley@users.noreply.github.com>
2024-02-16 15:44:34 -0600

    update 2
    push

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        self.cards.append(card)
+        self.cards.push(0)
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
         self.cards.pop(0)
